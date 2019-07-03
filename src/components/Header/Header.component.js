8a8d5f161bcd69e46c18b6474673a1779567468c
andrey_savchuk <andrey.m.savchuk@gmail.com>
2019-07-03 23:28:17 +0300

    changed header component structure

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -1,24 +1,57 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+// style
 import Header from './Header.styled';
+import LinkItem from './LinkItem.component';
 import SearchComponent from '../Search/Search.component';
 
-const HeaderComponent = () => (
-  <Header.Wrapper>
-    <Header.LinkItem>
-      <Link to="/">Home</Link>
-    </Header.LinkItem>
-    <Header.LinkItem>
-      <Link to="/products">Products</Link>
-    </Header.LinkItem>
-    <Header.LinkItem>
-      <Link to="/basket">Basket</Link>
-    </Header.LinkItem>
-    <Header.LinkItem>
-      <Link to="/registration">Registration</Link>
-    </Header.LinkItem>
-    <SearchComponent />
-  </Header.Wrapper>
-);
+const links = [
+  {
+    link: '/',
+    text: 'Home'
+  },
+  {
+    link: '/products',
+    text: 'Products'
+  },
+  {
+    link: '/basket',
+    text: 'Basket'
+  },
+  {
+    link: '/registration',
+    text: 'Registration'
+  }
+];
+
+class HeaderComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = { current: '' };
+  }
+
+  stateHandler = value => {
+    this.setState(() => { return { current: value }; });
+  };
+
+  render() {
+    const renderedLinks = links.map((link, index) => (
+      <>
+        <LinkItem
+          link={link}
+          key={index.toString()}
+          styleHandler={this.stateHandler}
+        />
+      </>
+    ));
+
+    return (
+      <Header.Wrapper>
+        {renderedLinks}
+        <SearchComponent />
+      </Header.Wrapper>
+    );
+  }
+}
 
 export default HeaderComponent;
